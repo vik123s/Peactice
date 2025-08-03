42192543daaed02f314d1343f8d7b2aadbbc6522
unknown <vikashcool97@gmail.com>
2025-08-04 00:28:06 +0530

    fifth comnit

---
+++ project999/src/main/java/Sample1/sample11.java
@@ -0,0 +1,20 @@
+package Sample1;
+
+public class sample1 {
+	
+	String name;
+	int age;
+	String school;
+	
+	void printdata() {
+		
+		System.out.println("name "  +name);
+		System.out.println("age  " +age);
+		System.out.println("school " +school);
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
